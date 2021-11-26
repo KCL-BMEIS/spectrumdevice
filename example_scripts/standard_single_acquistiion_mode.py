bf215a852ac3baa27d8810a4afbdba30c2a291e5
crnbaker <christian.baker@kcl.ac.uk>
2021-11-26 13:01:44 +0000

    Updates to standard single example script

--- example_scripts/standard_single_acquistiion_mode.py
+++ example_scripts/standard_single_acquistiion_mode.py
@@ -1,25 +1,36 @@
 import matplotlib.pyplot as plt
+from matplotlib import figure
 from matplotlib.pyplot import plot, show
-from numpy import savetxt
+from numpy import arange, savetxt
 
 from pyspecde.hardware_model.spectrum_star_hub import spectrum_star_hub_factory
 from pyspecde.sdk_translation_layer import (
     AcquisitionMode,
-    TriggerSource,
+    ExternalTriggerMode, TriggerSource,
 )
 
 # Choose configuration
 device_ip = "169.254.142.75"
-window_length_samples = 1000
+window_length_seconds = 10e-6
+sample_rate_hz = 40e6
 acquisition_timeout_ms = 1000
+trigger_level_mv = 1000
+vertical_range_mv = 200
 save_output = True
-save_dir = "D:\\pa_beacon_data\\"
-enabled_channels = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+save_dir = "C:\\3d_unt_pa_beacon\\"
+enabled_channels = [8, 16]  #, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+
+window_length_samples = int(sample_rate_hz * window_length_seconds)
 
 # Apply configuration
 netbox = spectrum_star_hub_factory(device_ip, 2, 1)
+for ch in netbox.channels:
+    ch.set_vertical_range_mv(vertical_range_mv)
+netbox.set_sample_rate_hz(int(sample_rate_hz))
 netbox.set_enabled_channels(enabled_channels)
-netbox.set_trigger_sources([TriggerSource.SPC_TMASK_SOFTWARE])
+netbox.set_trigger_sources([TriggerSource.SPC_TMASK_EXT0])
+netbox.set_external_trigger_mode(ExternalTriggerMode.SPC_TM_POS)
+netbox.set_external_trigger_level_mv(trigger_level_mv)
 netbox.set_acquisition_mode(AcquisitionMode.SPC_REC_STD_SINGLE)
 netbox.set_acquisition_length_samples(window_length_samples)
 netbox.set_post_trigger_length_samples(window_length_samples)
@@ -39,7 +50,10 @@ netbox.disconnect()
 
 # Plot waveforms
 for wfm, channel_num in zip(acquired_waveforms, netbox.enabled_channels):
-    plot(wfm, label=str(channel_num))
+    t = 1e6 * arange(len(wfm)) * (1 / sample_rate_hz)
+    plt.figure()
+    plot(t[100:], wfm[100:], label=str(channel_num))
+    plt.xlabel('Time (us)')
     if save_output:
         savetxt(save_dir + str(channel_num) + ".txt", wfm)  # noqa
 plt.legend()
