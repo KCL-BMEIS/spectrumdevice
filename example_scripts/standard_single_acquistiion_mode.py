5c8a6d1fdc8c647f48a103ffd197686cd3ef3d9e
crnbaker <christian.baker@kcl.ac.uk>
2021-11-29 09:40:18 +0000

    Changes to standard single mode script including setting input termination impedance

--- example_scripts/standard_single_acquistiion_mode.py
+++ example_scripts/standard_single_acquistiion_mode.py
@@ -9,16 +9,20 @@ from pyspecde.sdk_translation_layer import (
 )
 
 # Choose configuration
+from third_party.specde.py_header.regs import SPC_50OHM0, SPC_50OHM1, SPC_50OHM2, SPC_50OHM3, SPC_50OHM4, SPC_50OHM5, \
+    SPC_50OHM6, \
+    SPC_50OHM7
+
 device_ip = "169.254.142.75"
-window_length_seconds = 10e-6
-num_averages = 5
-plot_crop_seconds = 0e-6
+window_length_seconds = 40e-6
+num_averages = 1024
+plot_crop_seconds = 1e-6
 sample_rate_hz = 40e6
 acquisition_timeout_ms = 1000
 trigger_level_mv = 1000
 vertical_range_mv = 200
 save_output = True
-save_dir = "C:\\3d_unt_pa_beacon\\unt_lab_siggen_debug_261121\\"
+save_dir = "C:\\3d_unt_pa_beacon\\mengjie_lab_261121\\translated_25_mm\\0 degrees\\2_z=10mm_translation=10mm_5.5kHz_aligned_1024avgs\\"
 enabled_channels = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 
 window_length_samples = int(sample_rate_hz * window_length_seconds)
@@ -37,6 +41,24 @@ netbox.set_acquisition_mode(AcquisitionMode.SPC_REC_STD_SINGLE)
 netbox.set_acquisition_length_samples(window_length_samples)
 netbox.set_post_trigger_length_samples(window_length_samples)
 netbox.set_timeout_ms(acquisition_timeout_ms)
+
+netbox._child_cards[0].set_spectrum_api_param(SPC_50OHM0, 0)
+netbox._child_cards[0].set_spectrum_api_param(SPC_50OHM1, 0)
+netbox._child_cards[0].set_spectrum_api_param(SPC_50OHM2, 0)
+netbox._child_cards[0].set_spectrum_api_param(SPC_50OHM3, 0)
+netbox._child_cards[0].set_spectrum_api_param(SPC_50OHM4, 0)
+netbox._child_cards[0].set_spectrum_api_param(SPC_50OHM5, 0)
+netbox._child_cards[0].set_spectrum_api_param(SPC_50OHM6, 0)
+netbox._child_cards[0].set_spectrum_api_param(SPC_50OHM7, 0)
+
+netbox._child_cards[1].set_spectrum_api_param(SPC_50OHM0, 0)
+netbox._child_cards[1].set_spectrum_api_param(SPC_50OHM1, 0)
+netbox._child_cards[1].set_spectrum_api_param(SPC_50OHM2, 0)
+netbox._child_cards[1].set_spectrum_api_param(SPC_50OHM3, 0)
+netbox._child_cards[1].set_spectrum_api_param(SPC_50OHM4, 0)
+netbox._child_cards[1].set_spectrum_api_param(SPC_50OHM5, 0)
+netbox._child_cards[1].set_spectrum_api_param(SPC_50OHM6, 0)
+netbox._child_cards[1].set_spectrum_api_param(SPC_50OHM7, 0)
 
 all_waveforms = []
 
@@ -68,7 +90,8 @@ for wfm, channel_num in zip(mean_waveforms, netbox.enabled_channels):
     dt = 1 / sample_rate_hz
     plot_crop_samples = int(plot_crop_seconds / dt)
     t = 1e6 * arange(len(wfm)) * dt
+    plt.figure()
     plot(t[plot_crop_samples:], wfm[plot_crop_samples:], label=str(channel_num))
     plt.xlabel('Time (us)')
-plt.legend()
+# plt.legend()
 show()
