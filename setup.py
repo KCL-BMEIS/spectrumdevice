ac5b066e0231558c5d5e8f51e7434c400da9cc26
crnbaker <christian.baker@kcl.ac.uk>
2022-01-05 15:22:57 +0000

    Replaced setup.py with .cfg and pyproject.toml

--- setup.py
+++ setup.py
@@ -1,26 +1,4 @@
-from setuptools import find_packages, setup
+import setuptools
 
-test_deps = ["pytest==6.2.5"]
-
-dev_deps = ["flake8==4.0.", "flake8-bugbear==21.9.2", "black==21.10b0", "mypy==0.910"]
-
-doc_deps = ["pdoc==8.0.1"]
-
-examples_deps = ["matplotlib==3.5.0"]
-
-setup(
-    name="spectrumdevice",
-    package_data={"spectrumdevice": ["py.typed"]},
-    packages=find_packages(exclude=['tests']),
-    version=0.1,
-    include_package_data=True,
-    install_requires=[
-        "numpy==1.21.4",
-    ],
-    extras_require={
-        "dev": dev_deps,
-        "test": test_deps,
-        "doc": doc_deps + examples_deps,
-        "examples": examples_deps
-    }
-)
+if __name__ == "__main__":
+    setuptools.setup()
