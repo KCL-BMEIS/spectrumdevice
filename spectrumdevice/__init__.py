5602c85653813e48fcb67b8550c2399bf4203172
francois joubert <fxa.joubert@gmail.com>
2022-01-06 09:20:37 +0100

    Add main module docstring.

--- spectrumdevice/__init__.py
+++ spectrumdevice/__init__.py
@@ -1,3 +1,17 @@
+"""
+`spectrumdevice` is a high-level, object-oriented Python API for controlling Spectrum Instruments digitisers.
+
+It can connect to individual digitisers or
+[StarHubs](https://spectrum-instrumentation.com/en/m4i-star-hub) (e.g. the
+[NetBox](https://spectrum-instrumentation.com/en/digitizernetbox)). `spectrumdevice` provides two classes
+`SpectrumCard` and `SpectrumStarHub` for controlling and receiving data from individual digitisers and StarHubs
+respectively.
+
+For quickstart information, please see the [README](https://github.com/KCL-BMEIS/spectrumdevice/blob/main/README.md).
+
+Examples can be found in the `example_scripts` module of the repository.
+"""
+
 # Christian Baker, King's College London
 # Copyright (c) 2021 School of Biomedical Engineering & Imaging Sciences, King's College London
 # Licensed under the MIT. You may obtain a copy at https://opensource.org/licenses/MIT.
