21c3668a41327f661f6f9d68b6b28317deed3557
crnbaker <christian.baker@kcl.ac.uk>
2022-01-06 10:20:12 +0000

    Update to docs introduction

--- spectrumdevice/__init__.py
+++ spectrumdevice/__init__.py
@@ -3,9 +3,15 @@
 
 It can connect to individual digitisers or
 [StarHubs](https://spectrum-instrumentation.com/en/m4i-star-hub) (e.g. the
-[NetBox](https://spectrum-instrumentation.com/en/digitizernetbox)). `spectrumdevice` provides two classes
-`SpectrumCard` and `SpectrumStarHub` for controlling and receiving data from individual digitisers and StarHubs
-respectively. Mock classes are also provided for testing software without drivers installed or hardware connected.
+[NetBox](https://spectrum-instrumentation.com/en/digitizernetbox)).
+
+The main module `spectrumdevice` provides two classes `SpectrumCard` and `SpectrumStarHub` for controlling and receiving
+data from individual digitisers and StarHubs respectively. Mock classes are also provided for testing software without
+drivers installed or hardware connected.
+
+The submodule [`spectrumdevice.settings`](https://kcl-bmeis.github.io/spectrumdevice/spectrumdevice/settings.html)
+provides Enums and Dataclasses wrapping the register values provided by the Spectrum API, to be used for configuring
+hardware and interpreting responses received from hardware.
 
 * [Source on GitHub](https://github.com/KCL-BMEIS/spectrumdevice)
 * [README including quickstart](https://github.com/KCL-BMEIS/spectrumdevice/blob/main/README.md)
