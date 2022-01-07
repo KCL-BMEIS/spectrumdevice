4740206d569421fa475e3054a63fc9e07f6ef7e3
crnbaker <christian.baker@kcl.ac.uk>
2022-01-07 22:17:25 +0000

    Added dynamic version number from versioneer to setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
 import setuptools
 
+import versioneer
+
 if __name__ == "__main__":
-    setuptools.setup()
+    setuptools.setup(version=versioneer.get_version(), cmdclass=versioneer.get_cmdclass())
