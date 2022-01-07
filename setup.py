0f77342249121b5d5c789a2cb885e67059c94411
crnbaker <christian.baker@kcl.ac.uk>
2022-01-07 23:06:16 +0000

    Removed dynamic versioning from setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,5 @@
 import setuptools
-from spectrumdevice._version import get_versions
-__version__ = get_versions()['version']
 
 
 if __name__ == "__main__":
-    setuptools.setup(version=get_versions()['version'])
+    setuptools.setup()
