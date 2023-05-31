b358f6f5c0d8f3ce34f59328533e155aa36e8cb0
crnbaker <christian.baker@kcl.ac.uk>
2023-05-31 11:46:58 +0100

    #13 - added error codes file to manifest and restructered project to use a src/ directory because it works better with setuptools package data configuration

--- linters_and_black.bat
+++ linters_and_black.bat
@@ -1,12 +1,12 @@
 echo 'Formatting code with black...'
-black --line-length 120 spectrumdevice
-black --line-length 120 tests
-black --line-length 120 example_scripts
+black --line-length 120 src/spectrumdevice
+black --line-length 120 src/tests
+black --line-length 120 src/example_scripts
 echo 'Running mypy...'
-mypy spectrumdevice
-mypy tests
-mypy example_scripts
+mypy src/spectrumdevice
+mypy src/tests
+mypy src/example_scripts
 echo 'Running flake8...'
-flake8 spectrumdevice
-flake8 tests
-flake8 example_scripts
+flake8 src/spectrumdevice
+flake8 src/tests
+flake8 src/example_scripts
