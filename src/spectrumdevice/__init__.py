1af8df8332e91402dc55138c34e7aad97c393ffd
crnbaker <christian.baker@kcl.ac.uk>
2023-06-02 16:22:49 +0100

    #26 - Updated reference docs

--- src/spectrumdevice/__init__.py
+++ src/spectrumdevice/__init__.py
@@ -1,22 +1,55 @@
 """
-`spectrumdevice` is a high-level, object-oriented Python library for controlling Spectrum Instrumentation digitisers.
+A high-level, object-oriented Python library for controlling Spectrum Instrumentation devices.
 
-It can connect to individual digitisers or
+`spectrumdevice` can connect to individual cards or
 [StarHubs](https://spectrum-instrumentation.com/en/m4i-star-hub) (e.g. the
-[NetBox](https://spectrum-instrumentation.com/en/digitizernetbox)).
+[NetBox](https://spectrum-instrumentation.com/en/digitizernetbox)). `spectrumdevice` provides the following classes
+for controlling devices:
 
-The main module `spectrumdevice` provides two classes `SpectrumDigitiserCard` and `SpectrumStarHub` for controlling and receiving
-data from individual digitisers and StarHubs respectively. Mock classes are also provided for testing software without
-drivers installed or hardware connected.
+### Hardware Classes
+| Name                       | Purpose                                                 |
+|----------------------------|---------------------------------------------------------|
+| `SpectrumDigitiserCard`    | Controlling individual digitiser cards                  |
+| `SpectrumDigitiserStarHub` | Controlling digitiser cards aggregated with a StarHub   |
+| `SpectrumAWGCard`          | Controlling individual AWG cards                        |
+| `SpectrumAWGStarHub`       | Controlling AWG cards aggregated with a StarHub         |
 
+### Mock Classes
+`spectrumdevice` also includes mock classes for testing software without drivers installed or hardware connected:
+
+| Name                           | Purpose                                             |
+|--------------------------------|-----------------------------------------------------|
+| `MockSpectrumDigitiserCard`    | Mocking individual digitiser cards                  |
+| `MockSpectrumDigitiserStarHub` | Mocking digitiser cards aggregated with a StarHub   |
+| `MockSpectrumAWGCard`          | Mocking individual AWG cards                        |
+| `MockSpectrumAWGStarHub`       | Mocking AWG cards aggregated with a StarHub         |
+
+### Abstract Classes
+The following abstract classes provide common implementations for methods whose functionality is identical across
+different Spectrum devices. They cannot be instantiated themselves, but are included here as they contain
+documentation for methods inherited by the concrete (and therefore instantiatable) classes above.
+
+| Name                           | Purpose                                             |
+|--------------------------------|-----------------------------------------------------|
+| `AbstractSpectrumDevice`       | Implements methods common to all devices            |
+| `AbstractSpectrumCard`         | Implements methods common to all card               |
+| `AbstractSpectrumStarHub`      | Implements methods common to all hubs               |
+| `AbstractSpectrumChannel`      | Implements methods common to all channels           |
+| `AbstractSpectrumDigitiser`    | Implements methods common to all digitisers         |
+| `AbstractSpectrumAWG`          | Implements methods common to all AWGs               |
+
+### Settings
 The submodule `spectrumdevice.settings` provides Enums and Dataclasses wrapping the register values provided by the
 Spectrum API, to be used for configuring hardware and interpreting responses received from hardware.
 
+### Resources
 * [Source on GitHub](https://github.com/KCL-BMEIS/spectrumdevice)
 * [README including quickstart](https://github.com/KCL-BMEIS/spectrumdevice/blob/main/README.md)
 * [Examples](https://github.com/KCL-BMEIS/spectrumdevice/tree/main/example_scripts)
 * [PyPi](https://pypi.org/project/spectrumdevice/)
 * [API reference documentation](https://kcl-bmeis.github.io/spectrumdevice/)
+
+### Reference Documentation
 """
 
 # Christian Baker, King's College London
@@ -28,6 +61,13 @@ from .devices.digitiser.digitiser_card import SpectrumDigitiserCard
 from .devices.digitiser.digitiser_channel import SpectrumDigitiserChannel
 from .devices.digitiser.digitiser_star_hub import SpectrumDigitiserStarHub
 from .devices.mocks import MockSpectrumDigitiserCard, MockSpectrumDigitiserStarHub
+from .devices.abstract_device import (
+    AbstractSpectrumDevice,
+    AbstractSpectrumCard,
+    AbstractSpectrumChannel,
+    AbstractSpectrumStarHub,
+)
+from .devices.digitiser.abstract_spectrum_digitiser import AbstractSpectrumDigitiser
 
 __all__ = [
     "SpectrumDigitiserChannel",
@@ -35,6 +75,11 @@ __all__ = [
     "SpectrumDigitiserStarHub",
     "MockSpectrumDigitiserCard",
     "MockSpectrumDigitiserStarHub",
+    "AbstractSpectrumDigitiser",
+    "AbstractSpectrumStarHub",
+    "AbstractSpectrumCard",
+    "AbstractSpectrumDevice",
+    "AbstractSpectrumChannel",
     "settings",
     "Measurement",
 ]
