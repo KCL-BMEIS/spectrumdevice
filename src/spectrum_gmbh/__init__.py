deaa2e6684af658ecb9211c0f69b3e23ef8dd440
crnbaker <christian.baker@kcl.ac.uk>
2024-01-15 14:56:55 +0000

    #43 updated pyspcm (required small hack to bring contents of new package py_header into spectrum_gmbh namespace)

--- src/spectrum_gmbh/__init__.py
+++ src/spectrum_gmbh/__init__.py
@@ -1,2 +1,7 @@
 # Files in this module are provided free of charge by Spectrum GMBH with no restrictions on use.
 # They have been included here to simplify installation of the spectrumdevice package.
+import sys
+
+from .py_header import regs, spcerr
+sys.modules[__name__ + ".regs"] = regs
+sys.modules[__name__ + ".spcerr"] = spcerr
